1049bbc01f833224144214892a6057f3697de6b4
Jade Tucker <jadeatucker@gmail.com>
2015-03-27 23:10:16 -0700

    Make variables more obvious and remove some clutter.

--- IRC/Source.cpp
+++ IRC/Source.cpp
@@ -21,20 +21,21 @@
 
 using namespace std;
 #define DEFAULT_BUFLEN 512
+#define DEFAULT_MAX_BUFLEN (DEFAULT_BUFLEN * 4)
 #define DEFAULT_PORT "6667"
 
-struct mystruct{
-	SOCKET clientsocket;
-	char clientname[16];
-	char totalrecvbuf[DEFAULT_BUFLEN * 4];
-	char tempbuf[DEFAULT_BUFLEN * 4];
+struct Client {
+	SOCKET socket;
+	char name[16];
+	char totalrecvbuf[DEFAULT_MAX_BUFLEN];
+	char tempbuf[DEFAULT_MAX_BUFLEN];
 	char recvbuf[DEFAULT_BUFLEN];
 };
 
-void closeclient(mystruct *client);
+void closeclient(Client *client);
 
-vector <mystruct*> clientarray;
-mystruct *temp_pointer;
+vector <Client*> clientarray;
+Client *client;
 char uservar[] = { 'U', 'S', 'E', 'R', '\0' };
 char char_check[5];
 SOCKET ClientSocket = INVALID_SOCKET;
@@ -53,9 +54,6 @@ int main()
 	struct addrinfo hints;
 
 	int iSendResult;
-
-	int recvbuflen = DEFAULT_BUFLEN;
-	int recvbuflenmain = DEFAULT_BUFLEN * 4;
 
 	// Initialize Winsock
 	iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
@@ -121,123 +119,114 @@ int main()
 
 		if (ClientSocket != INVALID_SOCKET)
 		{
-			temp_pointer = new mystruct;
-			memset(temp_pointer, 0, sizeof(mystruct));
-			temp_pointer->clientsocket = ClientSocket;
-			clientarray.push_back(temp_pointer);
-			temp_pointer = NULL;
+			client = new Client;
+			memset(client, 0, sizeof(Client));
+			client->socket = ClientSocket;
+			clientarray.push_back(client);
+			client = NULL;
 		}
 
-		int array_size = clientarray.size();
-
-		int i = 0;
-
 		// Receive until the peer shuts down the connection
-		if (clientarray.size() > 0)
+		for (int i = 0; i < clientarray.size(); i++)
 		{
-			for (i; i < array_size; i++)
+			iSendResult = 0;
+			client = clientarray.at(i);
+			
+			currentReceived = recv(client->socket, client->recvbuf, DEFAULT_BUFLEN, 0);
+
+			if (currentReceived > 0)
 			{
-				iSendResult = 0;
-
-				currentReceived = recv(clientarray.at(i)->clientsocket, clientarray.at(i)->recvbuf, recvbuflen, 0);
-
-				if (currentReceived > 0)
+				// First this part, a small security check. Making sure that i don't overflow
+				// totalrecvbuf. If the amount goes above DEFAULT_BUFLEN *4, make sure i don't actually
+				// overfill it.
+				if ((totalReceived + currentReceived) > DEFAULT_MAX_BUFLEN)
 				{
-					// First this part, a small security check. Making sure that i don't overflow
-					// totalrecvbuf. If the amount goes above DEFAULT_BUFLEN *4, make sure i don't actually
-					// overfill it.
-					if ((totalReceived + currentReceived) > recvbuflenmain)
+					currentReceived = DEFAULT_MAX_BUFLEN - totalReceived;
+				}
+
+				// This is the transfer from small buffer to big total buffer.
+				totalReceived += currentReceived;
+				strcat(client->tempbuf, client->recvbuf);
+				if (strchr(client->tempbuf, '\r'))
+				{
+					if (strncmp(uservar, client->tempbuf, strlen(uservar)))
 					{
-						currentReceived = recvbuflenmain - totalReceived;
+						strcat(client->totalrecvbuf, "Client#");
+						totalReceived += 7;
+						char sclientnum[33];
+						int clientnum = client->socket;
+						itoa(clientnum, sclientnum, 10);
+						int slength = strlen(sclientnum);
+						totalReceived += slength;
+						strcat(client->totalrecvbuf, sclientnum);
 					}
+					strncat(client->totalrecvbuf, client->tempbuf, totalReceived);
 
-					// This is the transfer from small buffer to big total buffer.
-					totalReceived += currentReceived;
-					strcat(clientarray.at(i)->tempbuf, clientarray.at(i)->recvbuf);
-					if (strchr(clientarray.at(i)->tempbuf, '\r'))
+					if (!strncmp(uservar, client->totalrecvbuf, strlen(uservar)))
 					{
-						if (strncmp(uservar, clientarray.at(i)->tempbuf, strlen(uservar)))
-						{
-							strcat(clientarray.at(i)->totalrecvbuf, "Client#");
-							totalReceived += 7;
-							char sclientnum[33];
-							int clientnum = clientarray.at(i)->clientsocket;
-							itoa(clientnum, sclientnum, 10);
-							int slength = strlen(sclientnum);
-							totalReceived += slength;
-							strcat(clientarray.at(i)->totalrecvbuf, sclientnum);
-						}
-						strncat(clientarray.at(i)->totalrecvbuf, clientarray.at(i)->tempbuf, totalReceived);
-
-						if (!strncmp(uservar, clientarray.at(i)->totalrecvbuf, strlen(uservar)))
-						{
-							int count = 5;
-							for (int j = 0; j < 16; j++, count++){
-								clientarray.at(i)->clientname[j] = clientarray.at(i)->totalrecvbuf[count];
-								if (clientarray.at(i)->totalrecvbuf[count] == '\r')
-								{
-									clientarray.at(i)->clientname[j] = '\0';
-									break;
-								}
+						int count = 5;
+						for (int j = 0; j < 16; j++, count++){
+							client->name[j] = client->totalrecvbuf[count];
+							if (client->totalrecvbuf[count] == '\r')
+							{
+								client->name[j] = '\0';
+								break;
 							}
-
-							int k = 0;
-							char char_check2;
-							do{
-								char_check2 = clientarray.at(i)->totalrecvbuf[k];
-								k++;
-							} while (char_check2 != '\r');
-							k--;
-
-							memcpy(&clientarray.at(i)->totalrecvbuf[k], " has joined the chat\r\n\0", 23);
-
-							totalReceived += 23;
 						}
-						for (int j = 0; j < array_size; j++){
-							if (i != j)
-								iSendResult = send(clientarray.at(j)->clientsocket, clientarray.at(i)->totalrecvbuf, totalReceived, 0);
-						}
-						printf("Bytes sent: %d\n", iSendResult);
-						ZeroMemory(clientarray.at(i)->totalrecvbuf, recvbuflenmain);
-						ZeroMemory(clientarray.at(i)->recvbuf, recvbuflen);
-						ZeroMemory(clientarray.at(i)->tempbuf, recvbuflenmain);
 
-						currentReceived = 0;
-						totalReceived = 0;
+						int k = 0;
+						char char_check2;
+						do{
+							char_check2 = client->totalrecvbuf[k];
+							k++;
+						} while (char_check2 != '\r');
+						k--;
+
+						memcpy(&client->totalrecvbuf[k], " has joined the chat\r\n\0", 23);
+
+						totalReceived += 23;
 					}
-				}
-				else if (currentReceived == 0)
-				{
-					closeclient(clientarray.at(i));
-					clientarray.erase(clientarray.begin() + i);
-					array_size = clientarray.size();
+					for (int j = 0; j < clientarray.size(); j++){
+						if (i != j)
+							iSendResult = send(clientarray.at(j)->socket, client->totalrecvbuf, totalReceived, 0);
+					}
+					printf("Bytes sent: %d\n", iSendResult);
+					ZeroMemory(client->totalrecvbuf, DEFAULT_MAX_BUFLEN);
+					ZeroMemory(client->recvbuf, DEFAULT_BUFLEN);
+					ZeroMemory(client->tempbuf, DEFAULT_MAX_BUFLEN);
 
-					totalReceived = 0;				
-				}
-				else if (currentReceived == SOCKET_ERROR && WSAGetLastError() != WSAEWOULDBLOCK)
-				{
-					printf("recv failed with error: %d\n", WSAGetLastError());
-
-					closeclient(clientarray.at(i));
-					clientarray.erase(clientarray.begin() + i);
-					array_size = clientarray.size();
-
+					currentReceived = 0;
 					totalReceived = 0;
 				}
+			}
+			else if (currentReceived == 0)
+			{
+				closeclient(client);
+				clientarray.erase(clientarray.begin() + i);
+
+				totalReceived = 0;				
+			}
+			else if (currentReceived == SOCKET_ERROR && WSAGetLastError() != WSAEWOULDBLOCK)
+			{
+				printf("recv failed with error: %d\n", WSAGetLastError());
+
+				closeclient(client);
+				clientarray.erase(clientarray.begin() + i);
+
+				totalReceived = 0;
 			}
 		}
 	} while (true);
 }
 
-void closeclient(mystruct *client)
+void closeclient(Client *client)
 {
-	int r;
-	if (client->clientname != NULL)
+	int r, j;
+	if (client->name != NULL)
 	{
-		int j;
-		for (j = 0; j < sizeof(client->clientname); j++){
-			client->totalrecvbuf[j] = client->clientname[j];
-			if (client->clientname[j] == '\0')
+		for (j = 0; j < sizeof(client->name); j++){
+			client->totalrecvbuf[j] = client->name[j];
+			if (client->name[j] == '\0')
 				break;
 		}
 		memcpy(&client->totalrecvbuf[j], " has left\r\n\0", 12);
@@ -245,16 +234,16 @@ void closeclient(mystruct *client)
 
 		for (int j = 0; j < clientarray.size(); j++){
 			if (&client != &clientarray.at(j))
-				r = send(clientarray.at(j)->clientsocket, client->totalrecvbuf, namesize, 0);
+				r = send(clientarray.at(j)->socket, client->totalrecvbuf, namesize, 0);
 		}
 		printf("Bytes sent: %d\n", r);
 	}
 
-	r = shutdown(client->clientsocket, SD_SEND);
+	r = shutdown(client->socket, SD_SEND);
 	if (r == SOCKET_ERROR) {
 		printf("shutdown failed with error: %d\n", WSAGetLastError());
 	}
-	closesocket(client->clientsocket);
+	closesocket(client->socket);
 	delete client;
 }
 
